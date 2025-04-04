924f5a162599283c17c789eb6ef1a686f54fc644
Gfarf <gfpalu@gmail.com>
2025-04-04 11:25:09 -0300

    começando os códigos

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from textnode import TextNode, TextType
+
+
+def main():
+    test_node = TextNode("primeiro teste", TextType.CODE, "www.testando.com" )
+    print(test_node)
+
+
+if __name__ == "__main__":
+    main()
+
